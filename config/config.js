2c683523c295e1c9c5f5b59356352ea0f3809051
Ahmad Sidik Rudini <sidikrudini16@gmail.com>
2023-03-05 23:09:33 +0700

    refactor database config for sequelize migration

--- config/config.js
+++ config/config.js
@@ -1,23 +1,41 @@
-{
-  "development": {
-    "username": "root",
-    "password": null,
-    "database": "database_development",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+const fs = require("fs");
+require("dotenv").config();
+
+module.exports = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "postgresql",
+    dialectOptions: {
+      bigNumberStrings: true,
+    },
   },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  test: {
+    username: process.env.TEST_DB_USER,
+    password: process.env.TEST_DB_PASSWORD,
+    database: process.env.TEST_DB_NAME,
+    host: process.env.TEST_DB_HOST,
+    port: process.env.TEST_DB_PORT,
+    dialect: "postgresql",
+    dialectOptions: {
+      bigNumberStrings: true,
+    },
   },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  }
-}
+  production: {
+    username: process.env.PROD_DB_USERNAME,
+    password: process.env.PROD_DB_PASSWORD,
+    database: process.env.PROD_DB_NAME,
+    host: process.env.PROD_DB_HOSTNAME,
+    port: process.env.PROD_DB_PORT,
+    dialect: "postgresql",
+    dialectOptions: {
+      bigNumberStrings: true,
+      // ssl: {
+      //   ca: fs.readFileSync(__dirname + "/mysql-ca-main.crt"),
+      // },
+    },
+  },
+};
