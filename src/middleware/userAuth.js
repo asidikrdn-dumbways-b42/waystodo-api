6fc8d08850c88bcbacce89b375a8bb74b0faf2ec
Ahmad Sidik Rudini <sidikrudini16@gmail.com>
2023-03-06 18:58:03 +0700

    dissable console.log

--- src/middleware/userAuth.js
+++ src/middleware/userAuth.js
@@ -4,13 +4,10 @@ const status = require("http-status");
 const userAuth = async (req, res, next) => {
   try {
     let token = req.headers.authorization;
-    // console.log(req.headers);
-    console.log("token from header", token);
     if (!token) {
       throw new Error("token not found");
     }
     token = token.replace("Bearer ", "");
-    console.log("token after clear the bearer", token);
 
     jwt.verify(token, process.env.JWT_SECRET, (err, payload) => {
       if (err) {
