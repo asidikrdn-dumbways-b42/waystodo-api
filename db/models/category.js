4c55774be169a38cfb140446028cc737f8e95849
Ahmad Sidik Rudini <sidikrudini16@gmail.com>
2023-03-07 02:33:09 +0700

    refactor db models & migration
    add paranoid for softdelete

--- db/models/category.js
+++ db/models/category.js
@@ -42,6 +42,7 @@ module.exports = (sequelize, DataTypes) => {
       sequelize,
       modelName: "Category",
       underscored: true,
+      paranoid: true,
     }
   );
   return Category;
--- db/models/todo.js
+++ db/models/todo.js
@@ -46,6 +46,7 @@ module.exports = (sequelize, DataTypes) => {
       sequelize,
       modelName: "Todo",
       underscored: true,
+      paranoid: true,
     }
   );
   return Todo;
